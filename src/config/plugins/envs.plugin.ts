28ac2936f4004ceca1f306207559de67381db557
Stiven Mosquera <123203349+StivenMosquera@users.noreply.github.com>
2024-02-12 01:01:39 -0500

    feat: token hubspot

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -4,4 +4,5 @@ import * as env from 'env-var';
 export const envs = {
     PROD: env.get('PROD').required().asBool(),
     PORT: env.get('PORT').required().asPortNumber(),
+    TOKEN_HUBSPOT: env.get('TOKEN_HUBSPOT').required().asString()
 }
