daf63a6251ac06c73887af08e241dd557b2ee898
Stiven Mosquera <123203349+StivenMosquera@users.noreply.github.com>
2024-02-17 07:20:57 -0500

    update: character infrastructure

--- src/infrastructure/characters/character.repository.impl.ts
+++ src/infrastructure/characters/character.repository.impl.ts
@@ -1,13 +1,14 @@
 import { CharacterRepository, CharacterDataSource, CharacterEntity } from '../../domain/characters';
 
-
 export class CharacterRepositoryImpl implements CharacterRepository {
     constructor(
         private readonly dataSource: CharacterDataSource,
     ) { }
+
     getTotalOfCharacters(): Promise<number> {
         return this.dataSource.getTotalOfCharacters();
     }
+
     getOddsCharacters(ids: number[]): Promise<CharacterEntity[]> {
         return this.dataSource.getOddsCharacters(ids);
     }
